846f7918c3b9f84eebb64c447b512a302795fe89
Alexhaoge <1810064@mail.nankai.edu.cn>
2020-11-14 22:09:12 +0800

    update vlstm

--- model/vlstm.py
+++ model/vlstm.py
@@ -1,5 +1,6 @@
 from torch import tensor
 import torch.nn as nn
+from torch.nn.modules.activation import ReLU
 from .downsample import DownSampleForLSTM
 import torch
 
@@ -23,25 +24,36 @@ class VanillaLSTM(nn.Module):
     def __init__(self,
                 input_size:int,
                 lstm_num_square:int=3,
+                embedding_size:int=16,
+                hidden_size:int=32,
                 layer_num:int=1,
-                dropout_rate:float=0.,
+                dropout_rate:float=0.5,
+                downsample_version:int=3,
                 upsample:bool=False):
         super(VanillaLSTM, self).__init__()
         self.input_size = input_size
         self.lstm_num_square = lstm_num_square
+        self.hidden_size = hidden_size
         # Downsample layer before LSTM
-        self.downsample = DownSampleForLSTM(input_size, lstm_num_square)
+        self.downsample = DownSampleForLSTM(input_size, lstm_num_square, downsample_version)
         # Create lstm grids
         self.lstms = nn.ModuleList()
         for _ in range(lstm_num_square):
             lstm_row = nn.ModuleList()
             for _ in range(lstm_num_square):
                 lstm_row.append(nn.LSTM(
-                    input_size = self.downsample.version,
-                    hidden_size = 1,
+                    input_size = embedding_size,
+                    hidden_size = hidden_size,
                     num_layers = layer_num,
                     dropout=dropout_rate))
             self.lstms.append(lstm_row)
+        # embedding layer after downsample
+        self.down_embed = nn.Embedding(downsample_version, embedding_size)
+        # ReLU and dropout unit
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+        # output FC layer
+        self.out_linear = nn.Linear(hidden_size, 1)
         
 
     def forward(self, _input):
@@ -53,6 +65,11 @@ class VanillaLSTM(nn.Module):
             output = output.cuda()
         for i in range(self.lstm_num_square):
             for j in range(self.lstm_num_square):
-                _tmp = self.lstms[i][j](downsample_out[:, :, i, j, :])[0]
-                output[:, :, i, j] = _tmp.squeeze()
+                # embed the output of downsample
+                _x = self.dropout(self.relu(
+                        self.down_embed(downsample_out[:, :, i, j, :])))
+                # throw the embeded _x into lstm
+                _tmp = self.lstms[i][j](_x)[0]
+                # get temperature by linear layer
+                output[:, :, i, j] = self.out_linear(_tmp.squeeze())
         return output
